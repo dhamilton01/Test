ac60483bd351e308e54862b6b11331dac13f2d0f
Daniel2820 <dhamilton-d11@ulster.ac.uk>
2018-01-22 17:03:13 +0000

    commit
    My changes

--- GitHubTest/src/githubtest/GitHubTest.java
+++ GitHubTest/src/githubtest/GitHubTest.java
@@ -16,14 +16,10 @@ public class GitHubTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("My Branch");
-<<<<<<< HEAD
         System.out.println("Second Branch");
         System.out.println("From laptop");
-=======
         System.out.println("New Cha\ngesd");
-        
         System.out.println("");
->>>>>>> a0b207e2b8ad49de99e5447eec8dfbde7a6aa9e4
     }
     
 }
